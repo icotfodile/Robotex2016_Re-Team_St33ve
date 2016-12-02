ca4cce571d00776a175aa3bfb89975ab5518b062
icotfodile <ikechukwu.ofodile@estcube.eu>
2016-12-02 21:49:30 +0200

    Add files via upload

---
+++ logic.py
@@ -0,0 +1,66 @@
+mainboard = ''
+
+
+class Logic:
+    leftT = 330
+    rightT = 310
+    goalMin = 110
+    goalMax = 200
+    goal_find_cnt = 0
+
+    def __init__(self, mainboard):
+        self.mainboard = mainboard
+
+    def isBallFound(self, balldetails):
+        return balldetails != [0, 0, 0]
+
+    def movetoball(self, balldetails):
+        if balldetails == [0,0,0]:
+            self.mainboard.circlearound(speed=55)
+        else:
+            self.mainboard.forwardspeed()
+            if balldetails[0] > self.leftT:
+                self.mainboard.turnleft(50)
+            elif balldetails[0] < self.rightT:
+                self.mainboard.turnright(50)
+            elif balldetails[0] >= self.rightT and balldetails[0] <= self.leftT:
+                self.mainboard.backwheel(0)
+                self.mainboard.forwardspeed()
+
+    def aimandshoot(self, goaldetails):
+        print ('goal:',goaldetails)
+        goal_scored = False
+
+        if goaldetails==[0, 0]:
+            self.mainboard.circlearound()
+        else:
+            self.mainboard.forwardspeed()
+            if goaldetails[0] > 325:
+                self.mainboard.turnleft()
+            elif goaldetails[0] < 315:
+                self.mainboard.turnright()
+            elif (goaldetails[0] > 315 and goaldetails[0] < 325):
+                self.mainboard.kick()
+                goal_scored = True
+        '''        if goaldetails == [0, 0]:
+            self.goal_find_cnt = 0
+            self.mainboard.circlearound(35)
+            #self.mainboard.turnleft(30)
+        else:
+            #self.mainboard.forwardspeed()
+            #if goaldetails[0] > self.goalMax:
+            #    self.mainboard.turnleft(40)
+            #elif goaldetails[0] < self.goalMin:
+            #    self.mainboard.turnright(40)
+            #elif (goaldetails[1] >= self.goalMin):
+            if goaldetails[0] < self.goalMin:
+                self.mainboard.forwardspeed(30)
+            elif (goaldetails[1] >= self.goalMin and goaldetails[1] <= self.goalMax):
+                self.goal_find_cnt += 1
+                if self.goal_find_cnt > 7:
+                    self.mainboard.kick()
+                    goal_scored = True
+            else:
+                self.goal_find_cnt = 0
+                self.mainboard.turnright(30)'''
+        return goal_scored
---
+++ mainboard_controller.py
@@ -0,0 +1,107 @@
+import serial
+import time
+from datetime import datetime
+
+
+class MainboardController:
+    port = "COM3"
+    motor = ""
+    ball_catch_cmd = "<bl>"
+    ball_release_cmd = "<b0>"
+    with_ball = False
+    # l+002r-112b+502
+
+    leftwheelbuf=0
+    rightwheelbuf=0
+    backwheelbuf=0
+
+
+    def __init__(self):
+        self.motor = serial.Serial(self.port, 9600, serial.EIGHTBITS, serial.PARITY_NONE, serial.STOPBITS_ONE)
+        self.currentforwardspeed = 0
+
+    def circlearound(self, speed=50):
+        self.motor.write('ca' + str(speed) + '\n')
+
+    def forwardspeed(self, speed=150):
+        self.motor.write('mf' + str(speed) + '\n')
+        self.currentforwardspeed = speed
+
+    def turnleft(self, speed=150):
+        self.backwheel(speed)
+
+    def turnright(self, speed=150):
+        self.backwheel(-speed)
+
+    def backwheel(self, speed=150):
+        self.motor.write('t' + str(speed) + '\n')
+        self.currentturningspeed = speed
+
+    def sendwheelcommand(self):
+        cmd= 'l' + str(self.leftwheelbuf).zfill(4) + 'r' + str(self.rightwheelbuf).zfill(4) + 'b' + str(self.backwheelbuf).zfill(4) + '\n'
+        self.motor.write(cmd)
+        print (cmd)
+
+    def dribbler_init(self):
+        #self.motor.write('d10\n')
+        self.motor.write('b0.2\n')
+
+    def dribbler_on(self):
+        #self.motor.write('d150\n')
+        self.motor.write('b6\n')
+
+    def charge_kicker(self):
+        self.motor.write('j\n')
+
+    def kick(self):
+        self.motor_shut_down()
+        self.charge_kicker()
+        time.sleep(2)
+        self.motor.write('l\n')
+        time.sleep(0.1)
+
+    def checkforball(self):
+        self.motor.write('i\n')
+        line = self.motor.readline().strip()
+        print(line)
+        return line == "<b1>"
+
+    def stopwheels(self):
+        self.fspeedbuf = 0
+        self.tspeedbuf = 0
+        self.sendwheelcommand()
+
+    def stopall(self):
+        self.fspeedbuf = 0
+        self.tspeedbuf = 0
+        self.sendwheelcommand()
+        self.dribbler_init()
+
+    def motor_shut_down(self, speed = 0):
+        self.motor.write("ca" + str(speed) + '\n')
+        time.sleep(0.05)
+        print("motors shut down")
+
+    def dribbler_shut_down(self, speed=0):
+        #self.motor.write('d0\n')
+        self.motor.write('b0\n')
+        time.sleep(0.05)
+        print("dribbler shut down")
+
+    def detect_ball_catch(self):
+        # Use this to bypass having to use the ID
+        time.sleep(2)
+        self.with_ball = True
+        return self.with_ball
+
+        # Use the lines below once you confirm the IR works
+        self.with_ball = False
+        start = datetime.now()
+        timeout = 3
+        while False:
+            delta = datetime.now() - start
+            line = self.motor.readline().strip()
+            if line == self.ball_catch_cmd or delta.seconds > timeout:
+                self.with_ball = True
+                break
+        return self.with_ball
